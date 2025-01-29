bdf5c4c01b2643d8782182937642a8cf6d5f9c15
MahdiAjam <mahdiajam8754@gmail.com>
2025-01-29 13:33:38 +0330

    adding validation for email, password and username for registeration

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,13 +1,36 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
 class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(required=True, write_only=True)
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def create(self, validated_data):
+        del validated_data['password2']
         return User.objects.create_user(**validated_data)
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({'default': 'password must be match'})
+        return data
+
+    def validate_email(self, value):
+        if 'admin' in value:
+            raise serializers.ValidationError('admin can not be in email')
+        return value
+
+    def validate_username(self, value):
+        if 'admin' in value:
+            raise serializers.ValidationError('admin can not be in username')
+        return value
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
